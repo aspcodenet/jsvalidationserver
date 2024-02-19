7153ad4628955fc7b69913d51043fc53d8adb9e8
aspcodenet <stefan.holmberg@systementor.se>
2024-02-19 13:21:02 +0100

    a

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ const { check, validationResult } = require('express-validator');
 const { validateCreateUser } = require('./validators/userValidators.js');
 
 
+const {requireAuth} = require('./middlewares/requireAuth.js')
+
 app.use(express.json());
 //  ANROP -> 
 //    om ej inloggad  -> 401
-//     om ej valid -> 422
+//     om ej valid -> 422 (RETURN)
 //   vår funktion 
 
 
 //app.post('/hello', validateCreateUser , userController.createUser);
+
+
+app.get('/api/currentUserInfo', requireAuth, async (req,res)=>{
+    let result = {
+
+        name: 'Stefan',
+        id:11112,
+        age:52
+    }
+     res.json(result)
+});
+
+
 
 
 
